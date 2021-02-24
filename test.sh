55fa1792fa59b1e39704aa9f5554d2b00c925642
FSociety108 <f.society.69@yandex.com>
2021-02-25 00:13:53 +0530

    Scam Number 1

--- test.sh
+++ test.sh
@@ -1,9 +1,16 @@
-#/bin/bash
+#!/bin/sh
 
-touch scam.txt
 for i in {1..10000}
 do
+	echo "HI"
 	echo 1 scam
-	git add -A
-	git commit -m "Total Scam"
+done
+
+
+touch scam.txt
+for i in 1 2 3 4 5
+do
+  echo "Scam $i" scam.txt
+  git add -A
+  git commit -m "Scam Number $i"
 done
